e181613f46a3a0293c4b2f32edb42745ff23cf6b
ボイビ <>
2022-10-27 00:43:59 +0900

    色のデータを保存する

--- js/main.js
+++ js/main.js
@@ -11,3 +11,22 @@ colorFore.value = "#000000";
 colorFore.addEventListener('change', function () {
     outer.style.color = this.value;
 });
+$(function() {
+ 
+    // textボックスの変更時
+    $('input[name=name1]').change(function() {
+      // valueを取得
+      let val1 = $(this).val();
+      // p要素に出力（id:name1）
+      $('#name1').text(val1);
+    });
+  
+    // keyup時
+    $('input[name=name2]').change(function() {
+      // valueを取得
+      let val2 = $(this).val();
+      // p要素に出力（id:name2）
+      $('#name2').text(val2);
+    });
+  });
+  
